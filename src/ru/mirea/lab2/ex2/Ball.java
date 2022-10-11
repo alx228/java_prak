296859e2d8bd80c1f266b58e1c4cc2e10a476ff5
alx228 <lex.nevrid@yandex.ru>
2022-10-11 13:52:27 +0300

    day 3

---
+++ src/ru/mirea/lab2/ex2/Ball.java
@@ -0,0 +1,48 @@
+package ru.mirea.lab2.ex2;
+
+public class Ball {
+
+    private double x=0.0;
+
+    private double y=0.0;
+
+    public Ball(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public Ball(){
+        this.x=0.0;
+        this.y=0.0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setXY(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(double x0, double y0){
+        this.x += x0;
+        this.y += y0;
+    }
+    @Override
+    public String toString(){
+        return "Ball location (" + getX() + ", " + getY() + ")";
+    }
+}
---
+++ src/ru/mirea/lab2/ex2/TestBall.java
@@ -0,0 +1,29 @@
+package ru.mirea.lab2.ex2;
+import java.lang.*;
+import java.util.Scanner;
+public class TestBall {
+    public static void main(String[] args){
+        double x,y;
+        double x0,y0;
+        Ball bl=new Ball(2,1);
+        System.out.println(bl.toString());
+        System.out.println("x= " + bl.getX());
+        System.out.println("y= " + bl.getY());
+        Scanner source = new Scanner(System.in);
+        System.out.println("New x");
+        x=source.nextDouble();
+        bl.setX(x);
+        System.out.println("New y");
+        y=source.nextDouble();
+        bl.setY(y);
+        System.out.println(bl.toString());
+        System.out.println("Move on");
+        x0=source.nextDouble();
+        y0=source.nextDouble();
+        bl.move(x0,y0);
+        System.out.println(bl.toString());
+        System.out.println("Reset");
+        bl=new Ball();
+        System.out.println(bl.toString());
+    }
+}
